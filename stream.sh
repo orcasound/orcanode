8b2dc01ed09a18a3c19e9ff7accea225967fcd23
Paul Cretu <paul.s.cretu@gmail.com>
2017-09-16 17:20:57 -0700

    Increased alsa capture buffer size in script

--- stream.sh
+++ stream.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Make sure s3fs is mounted and start streaming
 
+# Increase alsa capture buffer size to 4096kb
+echo 4096 | tee /proc/asound/card0/pcm0c/sub0/prealloc
+# Make output s3fs dir
 mkdir -p /mnt/dev-streaming-orcasound-net
 s3fs -o default_acl=public-read dev-streaming-orcasound-net /mnt/dev-streaming-orcasound-net/ && \
   mkdir -p /mnt/dev-streaming-orcasound-net/$NODE_NAME && \
