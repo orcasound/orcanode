60f2bf2d0505f82f9c4e451ac0fdd3b8e172ce03
Paul Cretu <paul.s.cretu@gmail.com>
2017-11-07 19:29:24 +0000

    Fixed a few typos and added symlink for s3

--- stream.sh
+++ stream.sh
@@ -7,8 +7,9 @@ echo 4096 | tee /proc/asound/card0/pcm0c/sub0/prealloc
 mkdir -p /mnt/dev-streaming-orcasound-net
 s3fs -o default_acl=public-read dev-streaming-orcasound-net /mnt/dev-streaming-orcasound-net/
 mkdir -p /mnt/dev-streaming-orcasound-net/$NODE_NAME
-
+# symlink to s3 for output
+ln -s /mnt/dev-streaming-orcasound-net/$NODE_NAME /tmp/dash_output_dir
 # Start ALSA + ffmpeg
-ffmpeg -f alsa -i hw:$AUDIO_HW_ID -ac 1 -f mpegts udp://127.0.0.1:1234
+ffmpeg -f alsa -i hw:$AUDIO_HW_ID -ac 1 -f mpegts udp://127.0.0.1:1234 &
 # Start test engine live tools
 ./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234
