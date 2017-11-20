602cf2f2c093acdad5513bed266bda3b166a1f01
Paul Cretu <paul.s.cretu@gmail.com>
2017-11-20 13:03:10 -0800

    Make ffmpeg multithread

--- stream.sh
+++ stream.sh
@@ -8,6 +8,6 @@ mkdir -p /mnt/dev-streaming-orcasound-net/$NODE_NAME
 # symlink to s3 for output
 ln -s /mnt/dev-streaming-orcasound-net/$NODE_NAME /tmp/dash_output_dir
 # Start ALSA + ffmpeg
-ffmpeg -f alsa -i hw:$AUDIO_HW_ID -ac 1 -f mpegts udp://127.0.0.1:1234 &
+ffmpeg -t 0 -f alsa -i hw:$AUDIO_HW_ID -ac 1 -f mpegts udp://127.0.0.1:1234 &
 # Start test engine live tools
 ./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234
