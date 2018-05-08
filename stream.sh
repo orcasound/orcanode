f8fa414e3378725776b9dfa51acf01e46b13818e
Scott Veirs <sveirs@gmail.com>
2018-05-08 16:45:36 -0700

    Put final process in background for each section of node_type if statement.

--- stream.sh
+++ stream.sh
@@ -44,7 +44,7 @@ if [ $NODE_TYPE = "research" ]; then
 	ffmpeg -f alsa -ac $CHANNELS -ar $SAMPLE_RATE -i hw:$AUDIO_HW_ID -ac $CHANNELS -ar $SAMPLE_RATE -sample_fmt s32 -acodec flac \
        -f segment -segment_time 00:00:30.00 -strftime 1 "/tmp/flac/$NODE_NAME/%Y-%m-%d_%H-%M-%S_$NODE_NAME-$SAMPLE_RATE-$CHANNELS.flac" \
        -f segment -segment_list "/tmp/NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time 10 -segment_format \
-       mpegts -ar $STREAM_RATE -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts"
+       mpegts -ar $STREAM_RATE -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" &
 elif [ $NODE_TYPE = "debug" ]; then
         SAMPLE_RATE=48000
         STREAM_RATE=48000
@@ -53,14 +53,14 @@ elif [ $NODE_TYPE = "debug" ]; then
   	## Streaming DASH only via mpegts
   	ffmpeg -t 0 -f alsa -i hw:$AUDIO_HW_ID -ac $CHANNELS -f mpegts udp://127.0.0.1:1234 &
   	#### Stream with test engine live tools
-  	./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234
+  	./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234 &
 elif [ $NODE_TYPE = "hls-only" ]; then
   	SAMPLE_RATE=48000
   	STREAM_RATE=48000
 	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz with bitrate of 32 bits/sample..."
   	echo "Asking ffmpeg to stream only HLS segments at $STREAM_RATE Hz......" 
   	## Streaming HLS only via mpegts
-	ffmpeg -f alsa -ac $CHANNELS -ar $SAMPLE_RATE -i hw:$AUDIO_HW_ID -ac $CHANNELS -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time 10 -segment_format mpegts -ar $STREAM_RATE -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts"
+	ffmpeg -f alsa -ac $CHANNELS -ar $SAMPLE_RATE -i hw:$AUDIO_HW_ID -ac $CHANNELS -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time 10 -segment_format mpegts -ar $STREAM_RATE -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" &
 else
 	SAMPLE_RATE=48000
 	STREAM_RATE=48000
@@ -73,7 +73,7 @@ else
        mpegts -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" \
        -f mpegts -ac 2 udp://127.0.0.1:1234 &
 	#### Stream with test engine live tools
-	./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234
+	./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234 &
 fi
 
 
