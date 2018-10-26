ed00bec8d67017ee2b4d1a95fd69842d9fffae19
Scott Veirs <sveirs@gmail.com>
2018-10-25 22:35:33 -0700

    Set flac duration to 10 to fix ffmpeg error on flag segment length syntax.

--- stream.sh
+++ stream.sh
@@ -3,7 +3,7 @@
 # Some environmental variables set by local .env file; others here:
 
 SEGMENT_DURATION=10
-FLAC_DURATION=$( SEGMENT_DURATION )
+FLAC_DURATION=10
 LAG_SEGMENTS=6
 LAG=$(( LAG_SEGMENTS*SEGMENT_DURATION ))
 CHOP_M3U8_LINES=$(( LAG_SEGMENTS*(-2) ))
