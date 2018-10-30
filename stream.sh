b9c8280ed3f256c5a12a22852f1a0fe624fe7804
Scott Veirs <sveirs@gmail.com>
2018-10-30 12:59:18 -0700

    Correct comments in node_type research.

--- stream.sh
+++ stream.sh
@@ -69,8 +69,8 @@ echo "Node is named $NODE_NAME and is of type $NODE_TYPE"
 if [ $NODE_TYPE = "research" ]; then
 	SAMPLE_RATE=48000
 	STREAM_RATE=48000 ## Is it efficient to specify this so mpegts isn't hit by 4x the uncompressed data?
-	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz with bitrate of 32 bits/sample..."
-	echo "Asking ffmpeg to write $FLAC_DURATION second $SAMPLE_RATE Hz FLAC files..." 
+	echo "Sampling from $AUDIO_HW_ID at $SAMPLE_RATE Hz..."
+	echo "Asking ffmpeg to write HLS and $FLAC_DURATION second $SAMPLE_RATE Hz FLAC files..." 
 	## Streaming HLS with FLAC archive via /tmp (and rsync)
 	##nice -n -10 ffmpeg -f alsa -ac $CHANNELS -ar $SAMPLE_RATE -thread_queue_size 1024 -i hw:$AUDIO_HW_ID -ac $CHANNELS -ar $SAMPLE_RATE -sample_fmt s32 -acodec flac \
        -f segment -segment_time "00:00:$FLAC_DURATION.00" -strftime 1 "/tmp/flac/$NODE_NAME/%Y-%m-%d_%H-%M-%S_$NODE_NAME-$SAMPLE_RATE-$CHANNELS.flac" \
