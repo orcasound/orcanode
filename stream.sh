b9d2b1f9dbc8386852bd9de1793773bdcf4a0dd6
Stephen Hicks <mcshicks@gmail.com>
2019-11-28 07:53:41 -0800

    added loopback, fixed hls-only

--- stream.sh
+++ stream.sh
@@ -19,8 +19,12 @@ echo $timestamp > /tmp/$NODE_NAME/latest.txt
 #  Setup jack 
 echo @audio - memlock 256000 >> /etc/security/limits.conf
 echo @audio - rtprio 75 >> /etc/security/limits.co
-JACK_NO_AUDIO_RESERVATION=1 jackd -t 2000 -P 75 -d alsa -d hw:pisound -r 192000 -p 1024 -n 10 -s &
 
+if [ $NODE_TYPE = "research" ]; then
+        JACK_NO_AUDIO_RESERVATION=1 jackd -t 2000 -P 75 -d alsa -d hw:pisound -r 192000 -p 1024 -n 10 -s &
+else
+    JACK_NO_AUDIO_RESERVATION=1 jackd -t 2000 -P 75 -d alsa -d hw:pisound -r 48000 -p 1024 -n 10 -s &
+fi
 #### Generate stream segments and manifests, and/or lossless archive
 
 echo "Node started at $timestamp"
@@ -52,7 +56,7 @@ elif [ $NODE_TYPE = "debug" ]; then
 	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz with bitrate of 32 bits/sample..."
         echo "Asking ffmpeg to stream DASH via mpegts at $STREAM_RATE Hz..." 
   	## Streaming DASH only via mpegts
-  	nice -n -10 ffmpeg -t 0 -f alsa -i hw:$AUDIO_HW_ID -ac $CHANNELS -f mpegts udp://127.0.0.1:1234 &
+  	nice -n -10 ffmpeg -t 0 -f jack -i ffjack -f mpegts udp://127.0.0.1:1234 &
   	#### Stream with test engine live tools
 	## May need to adjust segment length in config_audio.json to match $SEGMENT_DURATION...
   	nice -n -7 ./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234 &
@@ -62,25 +66,25 @@ elif [ $NODE_TYPE = "hls-only" ]; then
 	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz..."
   	echo "Asking ffmpeg to stream only HLS segments at $STREAM_RATE Hz......" 
   	## Streaming HLS only via mpegts
-	nice -n -10 ffmpeg -f alsa -ac $CHANNELS -ar $SAMPLE_RATE -thread_queue_size 1024 -i hw:$AUDIO_HW_ID -ac $CHANNELS -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time $SEGMENT_DURATION -segment_format mpegts -ar $STREAM_RATE -ac 2 -threads 3 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" &
+	nice -n -10 ffmpeg -f jack -i ffjack -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time $SEGMENT_DURATION -segment_format mpegts -ar $STREAM_RATE -ac 2 -threads 3 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" &
 else
-	SAMPLE_RATE=48000
-	STREAM_RATE=48000
-	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz with bitrate of 32 bits/sample..."
-	echo "Asking ffmpeg to write $FLAC_DURATION second $SAMPLE_RATE Hz lo-res flac files while streaming in both DASH and HLS..." 
-	## Streaming DASH/HLS with low-res flac archive 
-	nice -n -10 ffmpeg -f alsa -ac $CHANNELS -ar $SAMPLE_RATE -i hw:$AUDIO_HW_ID -ac $CHANNELS -ar $SAMPLE_RATE -sample_fmt s32 -acodec flac \
-       -f segment -segment_time "00:00:$FLAC_DURATION.00" -strftime 1 "/tmp/flac/$NODE_NAME/%Y-%m-%d_%H-%M-%S_$NODE_NAME-$SAMPLE_RATE-$CHANNELS.flac" \
-       -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time $SEGMENT_DURATION -segment_format \
-       mpegts -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" \
-       -f mpegts -ac 2 udp://127.0.0.1:1234 &
-	#### Stream with test engine live tools
-	## May need to adjust segment length in config_audio.json to match $SEGMENT_DURATION...
-	nice -n -7 ./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234 &
+        echo "unsupported please pick hls-only or research"
 fi
 
+sleep 3
+jack_connect system:capture_1 ffjack:input_1
+jack_connect system:capture_2 ffjack:input_2
 
+if [ $NODE_LOOPBACK = "true" ]; then
+    jack_connect system:capture_1 system:playback_1
+    jack_connect system:capture_2 system:playback_2
+fi
 
-python3 upload_s3.py &
-python3 upload_flac_s3.py
+if [ $NODE_TYPE = "research" ]; then
+    python3 upload_s3.py &
+    python3 upload_flac_s3.py
+else
+    python3 upload_s3.py
+fi
 
+echo "all done"
