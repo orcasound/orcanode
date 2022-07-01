ce581e09e713f9dd9f6879941dfdf23c9bd15304
karan mishra <karan.m2704@gmail.com>
2022-07-01 23:27:27 +0530

    add prefix to .ts filenames

--- mseed/ooipypull.py
+++ mseed/ooipypull.py
@@ -6,13 +6,12 @@ import logging
 import logging.handlers
 import sys
 
-
 LOGLEVEL = logging.DEBUG
+PREFIX = os.environ["TIME_PREFIX"]
+DELAY = os.environ["DELAY_SEGMENT"]
 NODE = os.environ["NODE_NAME"]
 BASEPATH = os.path.join('/tmp', NODE)
 PATH = os.path.join(BASEPATH, 'hls')
-
-ffmpeg_cmd = "ffmpeg -i files.txt -flush_packets 0 -f segment -segment_list '/tmp/$NODE_NAME/hls/$timestamp/live.m3u8' -segment_list_flags -segment_time 10 -segment_format mpegts -ar 64000 -ac 1 -acodec aac '/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts'"
 
 log = logging.getLogger(__name__)
 
@@ -41,9 +40,9 @@ def fetchData(start_time, segment_length, end_time, node):
         datestr = start_time.strftime("%Y-%m-%dT%H-%M-%S-%f")[:-3]
         sub_directory = start_time.strftime("%Y-%m-%d")
         file_path = os.path.join(PATH, sub_directory)
-        wav_name = f"{datestr}.wav"
+        wav_name = "{date}.wav".format(date=datestr)
         hydrophone_data.wav_write(wav_name)   
-        ts_name = f"{datestr}.ts"
+        ts_name = "{prefix}{date}.ts".format(prefix=PREFIX, date=datestr)
         #os.system('ffmpeg -i {wavfile} -f mpegts -ar 64000 -acodec aac -ac 1 {tsfile}'.format(wavfile=wav_name, tsfile=ts_name))
         #os.system("ffmpeg -f concat -safe 0 -i files.txt -flush_packets 0 -f segment -segment_list '/tmp/$NODE/hls/$sub_directory/live.m3u8'")
         os.system("ffmpeg -i {wavfile} -f segment -segment_list './live.m3u8' -strftime 1 -segment_time 10 -segment_format mpegts -ac 1 -acodec aac {tsfile}".format(wavfile=wav_name, tsfile=ts_name))
@@ -60,11 +59,10 @@ def fetchData(start_time, segment_length, end_time, node):
 
 def _main():
 
-    start_time = datetime.datetime(2021, 4, 27)
-    end_time = datetime.datetime(2022, 4, 30)
     segment_length = datetime.timedelta(seconds = 10)
+    fixed_delay = datetime.timedelta(hours=8)
 
-    fetchData(start_time, segment_length, end_time, 'PC01A')
+    fetchData(datetime.datetime.utcnow() - fixed_delay, segment_length, datetime.datetime.utcnow(), 'PC01A')
 
 
 _main()
