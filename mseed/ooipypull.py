c2f5f5842cb65c449fd890f7932c46f72c734a62
karan mishra <karan.m2704@gmail.com>
2022-07-03 01:29:43 +0530

    fetch for lags

--- mseed/ooipypull.py
+++ mseed/ooipypull.py
@@ -29,6 +29,16 @@ def fetchData(start_time, segment_length, end_time, node):
     os.makedirs(PATH, exist_ok=True)
     while start_time < end_time:
         segment_end = min(start_time + segment_length, end_time)
+        #paths and filenames
+        datestr = start_time.strftime("%Y-%m-%dT%H-%M-%S-%f")[:-3]
+        sub_directory = start_time.strftime("%Y-%m-%d")
+        file_path = os.path.join(PATH, sub_directory)
+        wav_name = "{date}.wav".format(date=datestr)
+        ts_name = "{prefix}{date}.ts".format(prefix=PREFIX, date=datestr)     
+        #fetch if file doesn't already exist
+        if(os.path.exists(os.path.join(file_path, ts_name))):
+            print("EXISTS")
+            continue
         hydrophone_data = ooipy.request.hydrophone_request.get_acoustic_data(
             start_time, segment_end, node, verbose=True, data_gap_mode=2
         )
@@ -37,14 +47,7 @@ def fetchData(start_time, segment_length, end_time, node):
             start_time = segment_end
             continue
         print(f"data: {hydrophone_data}")
-        datestr = start_time.strftime("%Y-%m-%dT%H-%M-%S-%f")[:-3]
-        sub_directory = start_time.strftime("%Y-%m-%d")
-        file_path = os.path.join(PATH, sub_directory)
-        wav_name = "{date}.wav".format(date=datestr)
         hydrophone_data.wav_write(wav_name)   
-        ts_name = "{prefix}{date}.ts".format(prefix=PREFIX, date=datestr)
-        #os.system('ffmpeg -i {wavfile} -f mpegts -ar 64000 -acodec aac -ac 1 {tsfile}'.format(wavfile=wav_name, tsfile=ts_name))
-        #os.system("ffmpeg -f concat -safe 0 -i files.txt -flush_packets 0 -f segment -segment_list '/tmp/$NODE/hls/$sub_directory/live.m3u8'")
         os.system("ffmpeg -i {wavfile} -f segment -segment_list './live.m3u8' -strftime 1 -segment_time 10 -segment_format mpegts -ac 1 -acodec aac {tsfile}".format(wavfile=wav_name, tsfile=ts_name))
         if not os.path.exists(file_path):
            os.makedirs(file_path)
@@ -57,12 +60,26 @@ def fetchData(start_time, segment_length, end_time, node):
         f.write(sub_directory)
     shutil.copy('/root/latest.txt', BASEPATH) 
 
+
 def _main():
 
     segment_length = datetime.timedelta(seconds = 10)
     fixed_delay = datetime.timedelta(hours=8)
 
-    fetchData(datetime.datetime.utcnow() - fixed_delay, segment_length, datetime.datetime.utcnow(), 'PC01A')
+    while True:
+        end_time = datetime.datetime.utcnow()
+        start_time = end_time - datetime.timedelta(hours=8)
+
+        #near live fetch
+        fetchData(start_time, segment_length, end_time, 'PC01A')
+
+        #delayed fetch
+        fetchData(end_time-datetime.timedelta(hours=24), segment_length, end_time, 'PC01A')
+
+        start_time, end_time = end_time, datetime.datetime.utcnow()
+
+
+
 
 
 _main()
