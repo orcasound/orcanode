52882698aaebc959b5b82613737eb326a48816bc
karan mishra <karan.m2704@gmail.com>
2022-07-16 21:57:07 +0530

    writeManifest() fn for m3u8

--- mseed/ooipypull.py
+++ mseed/ooipypull.py
@@ -29,15 +29,26 @@ def fetchData(start_time, segment_length, end_time, node):
     os.makedirs(PATH, exist_ok=True)
     while start_time < end_time:
         segment_end = min(start_time + segment_length, end_time)
+
         #paths and filenames
         datestr = start_time.strftime("%Y-%m-%dT%H-%M-%S-%f")[:-3]
         sub_directory = start_time.strftime("%Y-%m-%d")
         file_path = os.path.join(PATH, sub_directory)
         wav_name = "{date}.wav".format(date=datestr)
         ts_name = "{prefix}{date}.ts".format(prefix=PREFIX, date=datestr) 
+
+        #create directory and edit latest.txt
         if not os.path.exists(file_path):
             os.mkdir(file_path)
-            os.remove('live.m3u8')
+            if not os.path.exists(os.path.join(BASEPATH, 'latest.txt')):
+                with open('latest.txt', 'x') as f:
+                    f.write(sub_directory)
+                    shutil.move('latest.txt', BASEPATH )
+            else:
+                with open(f'/{BASEPATH}/latest.txt', 'w') as f:
+                    f.write(sub_directory)
+
+
         #fetch if file doesn't already exist
         if(os.path.exists(os.path.join(file_path, ts_name))):
             print("EXISTS")
@@ -51,26 +62,34 @@ def fetchData(start_time, segment_length, end_time, node):
             continue
         print(f"data: {hydrophone_data}")
         hydrophone_data.wav_write(wav_name)   
-        #os.system('ffmpeg -i {wavfile} -f mpegts -ar 64000 -acodec aac -ac 1 {tsfile}'.format(wavfile=wav_name, tsfile=ts_name))
-        #os.system("ffmpeg -f concat -safe 0 -i files.txt -flush_packets 0 -f segment -segment_list '/tmp/$NODE/hls/$sub_directory/live.m3u8'")
-        #os.system("ffmpeg -i {wavfile} -f segment -segment_list './live.m3u8' -strftime 1 -segment_time 10 -segment_format mpegts -ac 1 -acodec aac {tsfile}".format(wavfile=wav_name, tsfile=ts_name))
-        os.system("ffmpeg -i {wavfile} -f hls -hls_time 10 -hls_list_size 0 -hls_flags -append_list -strftime 1 -hls_segment_type mpegts -ar 64000 -ac 1 -acodec aac hls_segment_filename {tsfile} live.m3u83u8".format(wavfile=wav_name, tsfile=ts_name))       
+        
+        writeManifest(wav_name, ts_name) 
+
         #move files to tmp for upload
         shutil.move(os.path.join('/root', ts_name), os.path.join(file_path, ts_name))
         shutil.copy('/root/live.m3u8', os.path.join(file_path, 'live.m3u8'))
         os.remove(wav_name)
         start_time = segment_end
 
-    with open('latest.txt', 'w') as f:
-        f.write(sub_directory)
-    shutil.move('/root/latest.txt', os.path.join(BASEPATH, 'latest.txt'))
+
+def writeManifest(wav_name, ts_name):
+    root_path = os.path.join('/root', 'live.m3u8')
+    if not os.path.exists(root_path):
+        os.system("ffmpeg -i {wavfile} -f segment -segment_list 'live.m3u8' -strftime 1 -segment_time 300 -segment_format mpegts -ac 1 -acodec aac {tsfile}".format(wavfile=wav_name, tsfile=ts_name))
+    else:
+        os.system('ffmpeg -i {wavfile} -f mpegts -ar 64000 -acodec aac -ac 1 {tsfile}'.format(wavfile=wav_name, tsfile=ts_name))
+        with open("live.m3u8", "a") as f:
+            f.write("#EXTINF:300 \n")
+            f.write(f'{ts_name} \n')
+
+        #os.system("ffmpeg -i {wavfile} -hls_playlist_type event -strftime 1 -hls_segment_type mpegts -ac 1 -acodec aac -hls_segment_filename {tsfile} -hls_time 1800 -hls_flags omit_endlist+append_list live.m3u8".format(wavfile=wav_name, tsfile=ts_name))      
 
 
 
 def _main():
 
-    segment_length = datetime.timedelta(seconds = 10)
-    fixed_delay = datetime.timedelta(hours=6)
+    segment_length = datetime.timedelta(minutes = 5)
+    fixed_delay = datetime.timedelta(hours=8)
 
     while True:
         end_time = datetime.datetime.utcnow()
@@ -83,8 +102,6 @@ def _main():
         fetchData(end_time-datetime.timedelta(hours=24), segment_length, end_time, 'PC01A')
 
         start_time, end_time = end_time, datetime.datetime.utcnow()
-
-
 
 
 
