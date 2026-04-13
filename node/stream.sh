0b5eb2b338b74606b33993407c6f49774054ff00
Dave Thaler <dthaler1968@gmail.com>
2026-04-13 12:18:14 -0700

    Use NODE_TYPE=dev-virt-s3 for github CI
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- node/stream.sh
+++ node/stream.sh
@@ -53,9 +53,7 @@ else
     JACKD_RT=""
 fi
 
-#  Setup jack 
-echo @audio - memlock 256000 >> /etc/security/limits.conf
-echo @audio - rtprio 75 >> /etc/security/limits.conf
+#  Setup jack
 JACK_NO_AUDIO_RESERVATION=1 jackd -t 2000 $JACKD_RT -d alsa -d hw:$AUDIO_HW_ID -r $SAMPLE_RATE -p 1024 -n 10 -s &
 
 #### Generate stream segments and manifests, and/or lossless archive
